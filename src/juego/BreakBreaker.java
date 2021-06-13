3824b2ed2c9401a7d1b785931c04355cafe90d5c
Alberto <xlock.cervera.alberto@gmail.com>
2021-06-13 18:02:38 +0200

    recuperar

--- src/juego/BreakBreaker.java
+++ src/juego/BreakBreaker.java
@@ -6,18 +6,25 @@ import java.awt.EventQueue;
 import java.awt.Graphics;
 import java.awt.Graphics2D;
 import java.awt.Toolkit;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
 import javax.swing.JFrame;
 import javax.swing.JPanel;
+import javax.swing.Timer;
 import javax.swing.border.EmptyBorder;
 
-public class BreakBreaker extends JFrame implements KeyListener {
+public class BreakBreaker extends JFrame implements KeyListener,ActionListener {
 	private JPanel contentPane;
 	private int ballx,bally;
 	private int barray;
 	private int barrax;
+	private int ballxdir=-10;
+	private int ballydir=-10;
+	private boolean start;
+	private Timer timer;
 	private Blockes brick;
 	public static void main(String[] args) {
 		EventQueue.invokeLater(new Runnable() {
@@ -52,6 +59,8 @@ public class BreakBreaker extends JFrame implements KeyListener {
 		brick=new Blockes(20,10);
 		addKeyListener(this);
 		
+		timer=new Timer(10,this);
+		timer.start();
 	}
    
 	public void paint(Graphics g) {
@@ -65,18 +74,39 @@ public class BreakBreaker extends JFrame implements KeyListener {
         g.setColor(Color.white);
         g.fillOval(ballx,bally,20,20);
         
-        repaint();
     }
+	
 	public void left() {
 		barrax=barrax-40;		
 	}
 	public void rigth() {
 		barrax=barrax+40;		
 	}
+	
+	@Override
+	public void actionPerformed(ActionEvent e) {
+		timer.start();
+		if(start) {
+			ballx=+ballxdir;
+			bally=+ballydir;
+			
+			if(ballx<0) {
+				ballxdir=-ballxdir;
+			}
+			if(bally<0) {
+				ballydir=-ballydir;
+			}
+			if(ballx>getHeight()) {
+				ballxdir=+ballxdir;
+			}
+		}
+		repaint();
+	}
+	
 	@Override
 	public void keyTyped(KeyEvent e) {
-		
 	}
+	
 	@Override
 	public void keyPressed(KeyEvent e) {
 		if(e.getKeyCode()==KeyEvent.VK_LEFT) {
@@ -85,13 +115,26 @@ public class BreakBreaker extends JFrame implements KeyListener {
 		if(e.getKeyCode()==KeyEvent.VK_RIGHT) {
 			rigth();
 		}
-		
+		if(e.getKeyCode()==KeyEvent.VK_SPACE) {
+			start=false;
+			ballx=getWidth()/2;
+			bally=getHeight()-100;
+			ballydir=-10;
+			ballxdir=-10;
+			
+		}
+		if(e.getKeyCode()==KeyEvent.VK_ENTER) {
+			if(ballx==getWidth()/2) {
+				start=true;
+			}
+		}
 	}
 
 	@Override
 	public void keyReleased(KeyEvent e) {
 		
 	}
+	
 	
 
 
